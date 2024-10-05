a8dd6befb2f8713a482dcb1ba47e1eea65c8dd1f
Edjek <edjek@hotmail.fr>
2024-10-05 23:32:16 +0200

    Session

--- public/index.php
+++ public/index.php
@@ -1,13 +1,17 @@
 <?php
 
-include_once '../src/Core/Autoloader.php';
-
 use App\Core\Autoloader;
 use App\Core\Database;
 use App\Core\Router;
+use App\Core\Session;
+
+require_once '../src/Core/Autoloader.php';
 
 Autoloader::register();
 Database::initConnection();
+
+$session = new Session();
+$session->start();
 
 $router = new Router();
 $router->execute();
--- src/Core/Session.php
+++ src/Core/Session.php
@@ -4,7 +4,7 @@ namespace App\Core;
 
 class Session
 {
-    public function __construct()
+    public function start()
     {
         if (!isset($_SESSION)) {
             session_start();
